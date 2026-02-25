8894174ac6caae4953ba1afa73f8ab150e03bd79
정신우 <nullhater92@gmail.com>
2026-02-25 19:11:57 +0900

    메인 이미지 반응형 처리_ver3

--- src/components/MainIntroSection.js
+++ src/components/MainIntroSection.js
@@ -3,14 +3,19 @@ import './MainIntroSection.css';
 import CardSlider from './CardSlider';
 
 // hero 일러스트 import
-import heroImage from './images/hero-illustration7.png';
+import heroImage from './images/hero-illustration7.png';       // 데스크톱용 (가로형)
+import heroImageMobile from './images/hero-illustration8.png'; // 모바일용 (세로형)
 
 const MainIntroSection = () => (
   <section className="intro-section">
     <div className="intro-inner">
       {/* 히어로(위 50%) */}
       <div className="hero">
-        <img src={heroImage} alt="사역 일러스트" className="hero-full-image" />
+        {/* picture 태그: 768px 이하면 모바일용 세로 이미지, 그 외엔 데스크톱용 가로 이미지 */}
+        <picture>
+          <source media="(max-width: 768px)" srcSet={heroImageMobile} />
+          <img src={heroImage} alt="사역 일러스트" className="hero-full-image" />
+        </picture>
       </div>
 
       {/* 카드 슬라이더(아래 50%) */}
