737fdf88c4df0b9ce031c20ca06bbcbc45d01198
정신우 <nullhater92@gmail.com>
2026-02-25 09:53:48 +0900

    중복실행 제거

--- src/index.js
+++ src/index.js
@@ -12,7 +12,5 @@ axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:8080
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
